6adf22b4558010bef9ca2cd6c71f37da31a19314
Milan Curcic <caomaco@gmail.com>
2018-04-25 11:10:05 -0400

    plotting wind speed time series

---
+++ plotting/plot_wind_speed.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+from ndbc import Station
+from datetime import datetime
+
+buoyid = 42002
+
+starttime = datetime(2017, 1, 1)
+endtime = datetime(2017, 2, 1)
+
+s = Station(buoyid, starttime, endtime)
+
+fig = plt.figure(figsize=(12, 6))
+ax = fig.add_subplot(111, xlim=(starttime, endtime), ylim=(0, 20))
+ax.tick_params(axis='both', labelsize=16)
+plt.plot(s.time, s.wspd, 'k-')
+plt.title('Measured wind speed at buoy ' + str(buoyid), fontsize=16)
+plt.ylabel('Wind speed [m/s]', fontsize=16)
+plt.grid(True)
+plt.savefig('wspd_' + str(buoyid) + '.png', dpi=100)
+plt.close(fig)
