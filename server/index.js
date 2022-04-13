802726d58e61787f2f1af8b6da1d0c2fdd3e5954
Ross Kramer <kramerro@us.ibm.com>
2022-04-13 15:15:43 -0400

    Added a parameterized get call to the index.js

--- server/index.js
+++ server/index.js
@@ -5,13 +5,6 @@ import express from 'express';
 // useful links:
 // https://github.com/gretzky/noaa-buoys
 // 
-// We created some construct objects to store our geolocation data in Buoys.js
-import { locationSuccess, locationError } from './Buoys';
-
-// If we can retrieve browser location we can pass the data to findNearestBuoys to give us a list of nearby buoys
-
-import buoys, { findNearestBuoys, Buoy } from "noaa-buoys";
-import navigator from 'navigator';
 import res from 'express/lib/response';
 import { stderr } from 'process';
 
@@ -28,13 +21,36 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+app.get('/', (req, res) => {
+  res.send("<html><head><b>Hey, it's a nodejs server doing nothing yet.</b></head></html>");
+});
+
 app.get('/api/:version', function(req, res) {
   res.send(req.params.version);
 });
 
-app.get('/', (req, res) => {
-  res.send("<html><head><b>Hey, it's a nodejs server doing nothing yet.</b></head></html>");
-});
+// This lets us pass the values in the url line as part of the get request query. Eg: URL/inputvals?buoy1=10001&buoy2=10002&buoy3=10003
+// The key names are not important in our case as we are stripping them out and only passing the values along to the weather_stats fortran app
+
+app.get('/inputvals', function(req, res){
+  console.log(req.query);
+  const buoys = Object.values(req.query);
+  console.log(buoys);
+  exec("./weather_stats " + buoys.join(" "), (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      res.end( `error: ${error.message}` );
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      res.end( `error: ${stderr}` );
+    }
+    res.end( `${stdout}` );
+  })
+  console.log(buoys.join(" "));
+})
+
+// This allows us to do a POST to the url and pass in a JSON to the api: {"buoys":["42002","42003","42020","42035"]}
 
 app.post('/api/buoys', function(req, res) {
   const data = (req.body.buoys);
@@ -55,14 +71,6 @@ app.post('/api/buoys', function(req, res) {
   console.log(data.join(" "));
 });
 
-app.listen(port, () =>
-	console.log(`Example app listening on port ${port}!`),
-);
-
-var execWeatherStats = function(appres, callback) {
-
-}
-
 app.get('/weather_stats', function (req, res) {
   exec("./weather_stats", (error, stdout, stderr) => {
       if (error) {
@@ -77,3 +85,7 @@ app.get('/weather_stats', function (req, res) {
       res.end( `${stdout}` );
   });
 })
+
+app.listen(port, () =>
+	console.log(`Example app listening on port ${port}!`),
+);
